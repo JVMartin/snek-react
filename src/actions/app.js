a9d582bd4e9a07c30b259ae7a586e5c3d23d0856
Jacob Martin <jacob_martin@homedepot.com>
2018-01-25 09:05:28 -0800

    Enforce min and max

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,17 +1,36 @@
+const MIN_WIDTH = 5;
+const MAX_WIDTH = 30;
+const MIN_HEIGHT = 5;
+const MAX_HEIGHT = 20;
+
 export const updateWidth = width => {
-    const parsedWidth = parseInt(width);
+    let newWidth = parseInt(width);
+
+    if (newWidth < MIN_WIDTH) {
+        newWidth = MIN_WIDTH;
+    }
+    else if (newWidth > MAX_WIDTH) {
+        newWidth = MAX_WIDTH;
+    }
 
     return {
         type: 'UPDATE_WIDTH',
-        width: (parsedWidth > 1) ? parsedWidth : 1,
+        width: newWidth,
     };
 };
 
 export const updateHeight = height => {
-    const parsedHeight = parseInt(height);
+    let newHeight = parseInt(height);
+
+    if (newHeight < MIN_HEIGHT) {
+        newHeight = MIN_HEIGHT;
+    }
+    else if (newHeight > MAX_HEIGHT) {
+        newHeight = MAX_HEIGHT;
+    }
 
     return {
         type: 'UPDATE_HEIGHT',
-        height: (parsedHeight > 1) ? parsedHeight : 1,
+        height: newHeight,
     };
 };
