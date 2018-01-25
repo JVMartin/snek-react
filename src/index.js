f8e8e55ce0f854dfe040ba9f05be390b6e61d3f5
Jacob Martin <jacob_martin@homedepot.com>
2018-01-25 10:49:28 -0800

    Apply thunk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import { rootReducer } from './reducers/root';
@@ -10,6 +11,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
     rootReducer,
+    applyMiddleware(thunk),
     // eslint-disable-next-line no-underscore-dangle
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
