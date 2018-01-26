ee1f8b9b6dd404e05ea610de22779f4c1882a85a
Jacob Martin <jacob_martin@homedepot.com>
2018-01-26 13:04:49 -0800

    Remove magic numbers

--- src/actions/intro.js
+++ src/actions/intro.js
@@ -41,26 +41,27 @@ export const startIntro = () => dispatch => {
 
     return tileMarch.then(() => new Promise(resolve => {
         const timeBetweenBlinks = 125;
-        setTimeout(() => dispatch({ type: 'HIDE_BOARD' }), timeBetweenBlinks);
-        setTimeout(() => dispatch({ type: 'SHOW_BOARD' }), timeBetweenBlinks * 2);
-        setTimeout(() => dispatch({ type: 'HIDE_BOARD' }), timeBetweenBlinks * 3);
-        setTimeout(() => dispatch({ type: 'SHOW_BOARD' }), timeBetweenBlinks * 4);
-        setTimeout(() => dispatch({ type: 'HIDE_BOARD' }), timeBetweenBlinks * 5);
-        setTimeout(() => dispatch({ type: 'SHOW_BOARD' }), timeBetweenBlinks * 6);
+        let action = 0;
+        setTimeout(() => dispatch({ type: 'HIDE_BOARD' }), timeBetweenBlinks * ++action);
+        setTimeout(() => dispatch({ type: 'SHOW_BOARD' }), timeBetweenBlinks * ++action);
+        setTimeout(() => dispatch({ type: 'HIDE_BOARD' }), timeBetweenBlinks * ++action);
+        setTimeout(() => dispatch({ type: 'SHOW_BOARD' }), timeBetweenBlinks * ++action);
+        setTimeout(() => dispatch({ type: 'HIDE_BOARD' }), timeBetweenBlinks * ++action);
+        setTimeout(() => dispatch({ type: 'SHOW_BOARD' }), timeBetweenBlinks * ++action);
 
         const snekCoords = {
             x: randomInt(1, WIDTH - 2),
             y: randomInt(1, HEIGHT - 2),
         };
 
-        setTimeout(() => dispatch({ type: 'DROP_SNEK', c: snekCoords }), timeBetweenBlinks * 7);
-        setTimeout(() => dispatch({ type: 'HIDE_TILES', coords: [snekCoords] }), timeBetweenBlinks * 8);
-        setTimeout(() => dispatch({ type: 'SHOW_TILES', coords: [snekCoords] }), timeBetweenBlinks * 9);
-        setTimeout(() => dispatch({ type: 'HIDE_TILES', coords: [snekCoords] }), timeBetweenBlinks * 10);
+        setTimeout(() => dispatch({ type: 'DROP_SNEK', c: snekCoords }), timeBetweenBlinks * ++action);
+        setTimeout(() => dispatch({ type: 'HIDE_TILES', coords: [snekCoords] }), timeBetweenBlinks * ++action);
+        setTimeout(() => dispatch({ type: 'SHOW_TILES', coords: [snekCoords] }), timeBetweenBlinks * ++action);
+        setTimeout(() => dispatch({ type: 'HIDE_TILES', coords: [snekCoords] }), timeBetweenBlinks * ++action);
         setTimeout(() => {
             dispatch({ type: 'SHOW_TILES', coords: [snekCoords] });
             dispatch({ type: 'RUN' });
             resolve();
-        }, timeBetweenBlinks * 11);
+        }, timeBetweenBlinks * ++action);
     }));
 };
